2de814c16c6957005c8fab4c370f252ffd3b06f5
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-05-20 19:50:25 +0300

    Create integer_operations.py

---
+++ Fundamentals/Data Types and Variables - Exercise/integer_operations.py
@@ -0,0 +1,6 @@
+first_numb = int(input())
+second_numb = int(input())
+third_numb = int(input())
+fourth_numb = int(input())
+result = int((first_numb + second_numb) / third_numb) * fourth_numb
+print(f'{result}')
