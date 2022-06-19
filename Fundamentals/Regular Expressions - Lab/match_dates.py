9963fef2886436587fcbdaf777f4d534caeb8360
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-06-19 22:54:42 +0300

    Create match_dates.py

---
+++ Fundamentals/Regular Expressions - Lab/match_dates.py
@@ -0,0 +1,10 @@
+import re
+
+dates = input()
+expression = r"(?P<day>\d{2})(?P<separator>[./-])(?P<month>[A-Z][a-z]{2})\2(?P<year>\d{4})"
+matches = re.finditer(expression, dates)
+for match in matches:
+    day = match.group('day')
+    month = match.group('month')
+    year = match.group('year')
+    print(f"Day: {day}, Month: {month}, Year: {year}")
