effe5fea8691a6423da1bc503b4004c0ae2ee4c0
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-06-28 20:25:39 +0300

    Create circle_area_and_perimeter.py

---
+++ MyFirstProject/Python Basic Fist Project/circle_area_and_perimeter.py
@@ -0,0 +1,7 @@
+import math
+
+value_r = float(input())
+formula_face_circle = (math.pi)*value_r**2
+formula_radius_circle = 2*(math.pi)*value_r
+print(f'{formula_face_circle:.2f}')
+print(f'{formula_radius_circle:.2f}')
