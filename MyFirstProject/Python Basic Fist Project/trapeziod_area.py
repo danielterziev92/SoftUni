ce8d7bc0539778abf4fe40aa779a0099431ee0c9
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-06-30 18:20:01 +0300

    Create trapeziod_area.py

---
+++ MyFirstProject/Python Basic Fist Project/trapeziod_area.py
@@ -0,0 +1,5 @@
+trapeziod_b1_side = float(input())
+trapeziod_b2_side = float(input())
+trapeziod_h_side = float(input())
+formula_area_of_trapeziod = (( trapeziod_b1_side + trapeziod_b2_side ) * trapeziod_h_side) / 2
+print(f'{formula_area_of_trapeziod:.2f}')
