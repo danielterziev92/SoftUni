47ec1a32b5e8d2fe1f9e382711555a66a02178be
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-07-06 20:14:20 +0300

    Create truck_tour.py

---
+++ Python Advanced/Lists as Stacks and Queues - Exercise/truck_tour.py
@@ -0,0 +1,8 @@
+pump_number = int(input())
+petrol_pumps = list()
+for _ in range(pump_number):
+    petrol = input().split()
+    amount_of_petrol_pump = int(petrol[0])
+    distance = int(input())
+    
+
