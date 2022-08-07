e0e0c44348b680107150c3ac261846f67360a32d
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-08-07 19:38:51 +0300

    Create numbers_from_n_to_1.js

---
+++ JS Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/numbers_from_n_to_1.js
@@ -0,0 +1,15 @@
+// function loopFromNTo1(numb){
+//     for(let i = numb; i > 0; i--){
+//         console.log(i);
+//     }
+// }
+function loopFromNTo1(numb){
+    while (numb>0){
+        console.log(numb);
+        numb --;
+    }
+}
+
+
+loopFromNTo1(5);
+loopFromNTo1(3);
