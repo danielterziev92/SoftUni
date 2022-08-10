cc5999724c388d559c59c0d1cb80407fa814d1cf
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-08-10 19:35:39 +0300

    Create multiplication_table.js

---
+++ JS Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/multiplication_table.js
@@ -0,0 +1,10 @@
+function multiplication(numb) {
+    numb = Number(numb);
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${numb} X ${i} = ${numb * i}`);
+    }
+
+}
+
+multiplication(5);
+multiplication(2);
