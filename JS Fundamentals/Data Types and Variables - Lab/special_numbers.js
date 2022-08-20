70d1c31cedda13fd53d7f214b1a2d57448ebc940
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-08-20 22:36:06 +0300

    Update special_numbers.js

--- JS Fundamentals/Data Types and Variables - Lab/special_numbers.js
+++ JS Fundamentals/Data Types and Variables - Lab/special_numbers.js
@@ -1,11 +1,21 @@
 function specialNumbers(numb) {
     for (let i = 1; i <= numb; i++) {
         let currentNumb = 0;
-        for(let ind = 0; ind < i.length; ind++){
-            currentNumb += i[ind]
+        if (i >= 10) {
+            for(let ind = 0; ind < i.toString().length; ind++){
+                let result = i.toString()[ind];
+                currentNumb += Number(result);
+            }
+        } else {
+            currentNumb += i;
         }
-        console.log(`${i} -> ${currentNumb}`)
+        let isSpecial = false;
+        if(currentNumb === 5 || currentNumb === 7 || currentNumb === 11){
+            isSpecial = true;
+        }
+        console.log(`${i} -> ${isSpecial.toString().charAt(0).toUpperCase() + isSpecial.toString().slice(1)}`);
     }
 }
 
 specialNumbers(15);
+specialNumbers(30);
