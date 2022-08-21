dec47666ad1bf9ce5cb8abc66accd94b06e15214
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-08-21 09:21:36 +0300

    Create lower_or_upper.js

---
+++ JS Fundamentals/Data Types and Variables - Exercise/lower_or_upper.js
@@ -0,0 +1,10 @@
+function checkCharacter(char){
+    if(char === char.toUpperCase()){
+        console.log('upper-case')
+    } else {
+        console.log('lower-case')
+    }
+}
+
+checkCharacter('L');
+checkCharacter('f');
