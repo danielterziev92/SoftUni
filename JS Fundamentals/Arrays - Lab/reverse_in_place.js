7a4913a8fa233d2340b72aefcd4203a8404dc1bc
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-08-23 16:18:24 +0300

    Create reverse_in_place.js

---
+++ JS Fundamentals/Arrays - Lab/reverse_in_place.js
@@ -0,0 +1,7 @@
+function reverseArray(array){
+    console.log(array.reverse().join(' '));
+}
+
+reverseArray(['a', 'b', 'c', 'd', 'e']);
+reverseArray(['abc', 'def', 'hig', 'klm', 'nop']);
+reverseArray(['33', '123', '0', 'dd']);
