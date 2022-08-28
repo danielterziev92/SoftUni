d1c5b73936cbf77c70ce9d980606f052224d6f6c
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-08-28 12:54:45 +0300

    Create math_power.js

---
+++ JS Fundamentals/Functions - Lab/math_power.js
@@ -0,0 +1,10 @@
+function power(numb, power){
+    let result = 1;
+    for(let i = result; i <= power; i++){
+        result *= numb
+    }
+    console.log(result);
+}
+
+power(2, 8);
+power(3, 4);
