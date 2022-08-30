dedb60d8bd09eab3562930cf118a01b48349a5da
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-08-30 22:58:17 +0300

    Create perfect_number.js

---
+++ JS Fundamentals/Functions - Exercise/perfect_number.js
@@ -0,0 +1,16 @@
+function isPerfectNumber(numb){
+    let result = [];
+    for(let i = 1; i < numb; i++){
+        if(numb % i === 0){
+            result.push(i);
+        }
+    }
+    let divisors = eval(result.join('+'));
+    divisors === numb
+        ? console.log('We have a perfect number!')
+        : console.log('It\'s not so perfect.');
+}
+
+isPerfectNumber(6);
+isPerfectNumber(28);
+isPerfectNumber(1236498);
