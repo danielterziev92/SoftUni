9245ec650b72b621cce23439868fbf23ede0a371
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-09-11 10:54:50 +0300

    Create substring.js

---
+++ JS Fundamentals/Text Processing - Lab/substring.js
@@ -0,0 +1,6 @@
+function solve(string, numbStartIndex, numbEndIndex){
+    console.log(string.substring(numbStartIndex, numbEndIndex+numbStartIndex));
+}
+
+solve('ASentence', 1, 8);
+solve('SkipWord', 4, 7);
