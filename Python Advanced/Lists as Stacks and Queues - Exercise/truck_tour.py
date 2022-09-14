ff9f9992139d3cc115f347aaf67ccf654eb39917
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-09-14 23:07:18 +0300

    Update truck_tour.py

--- Python Advanced/Lists as Stacks and Queues - Exercise/truck_tour.py
+++ Python Advanced/Lists as Stacks and Queues - Exercise/truck_tour.py
@@ -1,8 +1,21 @@
-pump_number = int(input())
-petrol_pumps = list()
-for _ in range(pump_number):
-    petrol = input().split()
-    amount_of_petrol_pump = int(petrol[0])
-    distance = int(input())
-    
+from collections import deque
 
+count_petrol_pumps = int(input())
+pumps = deque()
+for _ in range(count_petrol_pumps):
+    pumps.append([int(x) for x in input().split()])
+
+for index in range(count_petrol_pumps):
+    petrol_amount = 0
+    is_failed = False
+    for amount, distance in pumps:
+        petrol_amount += amount - distance
+        if petrol_amount < 0:
+            is_failed = True
+            break
+
+    if is_failed:
+        pumps.append(pumps.popleft())
+    else:
+        print(index)
+        break
