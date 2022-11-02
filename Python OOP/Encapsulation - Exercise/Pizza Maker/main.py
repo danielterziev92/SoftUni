b3ecbed611db48c9e0ba33d3fd3c966f15d1b3dc
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-11-02 18:32:42 +0200

    Update main.py

--- Python OOP/Encapsulation - Exercise/Pizza Maker/main.py
+++ Python OOP/Encapsulation - Exercise/Pizza Maker/main.py
@@ -1,6 +1,8 @@
 from project.dough import Dough
-# from project.pizza import Pizza
+from project.pizza import Pizza
 from project.topping import Topping
+
+d = Dough('asd', 'asd', 2.5)
 
 tomato_topping = Topping("Tomato", 60)
 print(tomato_topping.topping_type)
@@ -14,26 +16,26 @@ mozzarella_topping = Topping("Mozzarella", 80)
 print(mozzarella_topping.topping_type)
 print(mozzarella_topping.weight)
 
-# cheddar_topping = Topping("Cheddar", 150)
-#
-# pepperoni_topping = Topping("Pepperoni", 120)
-#
-# white_flour_dough = Dough("White Flour", "Mixing", 200)
-# print(white_flour_dough.flour_type)
-# print(white_flour_dough.weight)
-# print(white_flour_dough.baking_technique)
-#
-# whole_wheat_dough = Dough("Whole Wheat Flour", "Mixing", 200)
-# print(whole_wheat_dough.weight)
-# print(whole_wheat_dough.flour_type)
-# print(whole_wheat_dough.baking_technique)
-#
-# p = Pizza("Margherita", whole_wheat_dough, 2)
-#
-# p.add_topping(tomato_topping)
-# print(p.calculate_total_weight())
-#
-# p.add_topping(mozzarella_topping)
-# print(p.calculate_total_weight())
-#
-# p.add_topping(mozzarella_topping)
+cheddar_topping = Topping("Cheddar", 150)
+
+pepperoni_topping = Topping("Pepperoni", 120)
+
+white_flour_dough = Dough("White Flour", "Mixing", 200)
+print(white_flour_dough.flour_type)
+print(white_flour_dough.weight)
+print(white_flour_dough.baking_technique)
+
+whole_wheat_dough = Dough("Whole Wheat Flour", "Mixing", 200)
+print(whole_wheat_dough.weight)
+print(whole_wheat_dough.flour_type)
+print(whole_wheat_dough.baking_technique)
+
+p = Pizza("Margherita", whole_wheat_dough, 2)
+
+p.add_topping(tomato_topping)
+print(p.calculate_total_weight())
+
+p.add_topping(mozzarella_topping)
+print(p.calculate_total_weight())
+
+p.add_topping(mozzarella_topping)
