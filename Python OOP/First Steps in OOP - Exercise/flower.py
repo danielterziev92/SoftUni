4beb938a607eeee36b57e5e8a6202ba87efdf304
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-11-09 20:04:51 +0200

    Create flower.py

---
+++ Python OOP/First Steps in OOP - Exercise/flower.py
@@ -0,0 +1,28 @@
+class Flower:
+    def __init__(self, name, water_requirements):
+        self.name = name
+        self.water_requirements = water_requirements
+        self.quality = 0
+        self.is_happy = False
+
+    def water(self, quantity):
+        self.quality = quantity
+        if self.quality >= self.water_requirements:
+            self.is_happy = True
+        else:
+            self.is_happy = False
+
+    def status(self):
+        if self.is_happy:
+            return f'{self.name} is happy'
+        else:
+            return f'{self.name} is not happy'
+
+
+flower = Flower("Lilly", 100)
+flower.water(50)
+print(flower.status())
+flower.water(60)
+print(flower.status())
+flower.water(100)
+print(flower.status())
