5e4e3e1a29cf1a79380ac19003a4f3dd54c12c25
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-11-22 17:39:17 +0200

    Update main.py

--- Python OOP/Decorators - Exercise/Computer Store/main.py
+++ Python OOP/Decorators - Exercise/Computer Store/main.py
@@ -3,7 +3,7 @@ from project.computer_store_app import ComputerStoreApp
 computer_store = ComputerStoreApp()
 print(computer_store.build_computer("Laptop", "Apple", "Macbook", "Apple M1 Pro", 64))
 print(computer_store.warehouse)
-print(computer_store.build_computer('Desktop Computer', "ASDF", "ASDF", "AMD Ryzen 9 5950X", 128))
+print(computer_store.build_computer('Desktop Computer', "ASDF", "ASDF", "AMD Ryzen 7 5700G", 128))
 print(computer_store.sell_computer(10000, "Apple M1 Pro", 32))
 print(computer_store.profits)
 print(computer_store.sell_computer(3000, 'Apple M1 Max', 64))
