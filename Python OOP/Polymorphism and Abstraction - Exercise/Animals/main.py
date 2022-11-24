054f2f1a2f5eb00ec85c08dd9c074c70bb948f96
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-11-24 19:11:37 +0200

    Create main.py

---
+++ Python OOP/Polymorphism and Abstraction - Exercise/Animals/main.py
@@ -0,0 +1,11 @@
+from project.cat import Cat
+from project.dog import Dog
+from project.kitten import Kitten
+from project.tomcat import Tomcat
+
+kitten = Kitten("Kiki", 1)
+print(kitten.make_sound())
+print(kitten)
+cat = Cat("Johnny", 7, "Male")
+print(cat.make_sound())
+print(cat)
