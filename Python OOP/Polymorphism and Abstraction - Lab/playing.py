8c2d5365aaaf31923e859a1d0bb9b23292ac1d93
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-12-05 20:43:56 +0200

    Create playing.py

---
+++ Python OOP/Polymorphism and Abstraction - Lab/playing.py
@@ -0,0 +1,21 @@
+def start_playing(inst):
+    return inst.play()
+
+
+class Guitar:
+    def play(self):
+        return "Playing the guitar"
+
+
+guitar = Guitar()
+
+print(start_playing(guitar))
+
+
+class Children:
+    def play(self):
+        return "Children are playing"
+
+
+children = Children()
+print(start_playing(children))
