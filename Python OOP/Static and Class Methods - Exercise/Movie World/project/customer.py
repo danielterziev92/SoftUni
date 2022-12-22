2f9d5c9b1affe6a9cb55aed647a44bbb23f7835d
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-12-22 19:39:35 +0200

    Create customer.py

---
+++ Python OOP/Static and Class Methods - Exercise/Movie World/project/customer.py
@@ -0,0 +1,10 @@
+class Customer:
+    def __init__(self, name: str, age: int, id: int):
+        self.name = name
+        self.age = age
+        self.id = id
+        self.rented_dvds = list()
+
+    def __repr__(self):
+        return f"{self.id}: {self.name} of age {self.age} has {len(self.rented_dvds)} rented DVD's " \
+               f"({', '.join([dvd.name for dvd in self.rented_dvds])})"
