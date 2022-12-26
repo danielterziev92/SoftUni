a295dd24b14dfb777b80a0d207b0dd9579e18b2f
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-12-26 20:02:46 +0200

    Create workers_updated.py

---
+++ Python OOP/SOLID Exercises/workers_updated.py
@@ -0,0 +1,93 @@
+from abc import ABC, abstractmethod
+import time
+
+
+class Workable(ABC):
+    @abstractmethod
+    def work(self):
+        pass
+
+
+class Eatable(ABC):
+    @abstractmethod
+    def eat(self):
+        pass
+
+
+class Worker(Workable, Eatable):
+    def work(self):
+        print("I'm normal worker. I'm working.")
+
+    def eat(self):
+        print("Lunch break....(5 secs)")
+        time.sleep(5)
+
+
+class SuperWorker(Workable, Eatable):
+    def work(self):
+        print("I'm super worker. I work very hard!")
+
+    def eat(self):
+        print("Lunch break....(3 secs)")
+        time.sleep(3)
+
+
+class Robot(Workable):
+    def work(self):
+        print("I'm a robot. I'm working....")
+
+
+class Manager(ABC):
+    def __init__(self):
+        self.worker = None
+
+    @abstractmethod
+    def set_worker(self, worker):
+        pass
+
+
+class WorkManager(Manager):
+    def __init__(self):
+        super().__init__()
+
+    def set_worker(self, worker):
+        assert isinstance(worker, Workable), f"`worker` must be of type {Workable}"
+
+        self.worker = worker
+
+    def manage(self):
+        self.worker.work()
+
+
+class BreakManager(Manager):
+    def __init__(self):
+        super().__init__()
+
+    def set_worker(self, worker):
+        assert isinstance(worker, Eatable), f"`worker` must be of type {Eatable}"
+
+        self.worker = worker
+
+    def lunch_break(self):
+        self.worker.eat()
+
+
+work_manager = WorkManager()
+break_manager = BreakManager()
+work_manager.set_worker(Worker())
+break_manager.set_worker(Worker())
+work_manager.manage()
+break_manager.lunch_break()
+
+work_manager.set_worker(SuperWorker())
+break_manager.set_worker(SuperWorker())
+work_manager.manage()
+
+break_manager.lunch_break()
+work_manager.set_worker(Robot())
+work_manager.manage()
+try:
+    break_manager.set_worker(Robot())
+    break_manager.lunch_break()
+except:
+    pass
