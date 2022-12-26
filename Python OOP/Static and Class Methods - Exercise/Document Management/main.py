cc24283848589f711cd1c1b2b2c85799c6e87fe3
Daniel Terziev <105807682+danielterziev92@users.noreply.github.com>
2022-12-26 20:02:41 +0200

    Create main.py

---
+++ Python OOP/Static and Class Methods - Exercise/Document Management/main.py
@@ -0,0 +1,23 @@
+from project.category import Category
+from project.document import Document
+from project.storage import Storage
+from project.topic import Topic
+
+c1 = Category(1, "work")
+t1 = Topic(1, "daily tasks", "C:\\work_documents")
+d1 = Document(1, 1, 1, "finilize project")
+
+d1.add_tag("urgent")
+d1.add_tag("work")
+
+storage = Storage()
+storage.add_category(c1)
+storage.add_topic(t1)
+storage.add_document(d1)
+storage.edit_category(1, 'new_work')
+
+
+print(c1)
+print(t1)
+print(storage.get_document(1))
+print(storage)
