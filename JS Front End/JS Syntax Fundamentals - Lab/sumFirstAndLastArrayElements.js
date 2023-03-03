ffae01b4b4242ee25358272cf9e6a6df1c71375e
danielterziev92 <105807682+danielterziev92@users.noreply.github.com>
2023-03-03 23:21:13 +0200

    Create sumFirstAndLastArrayElements.js

---
+++ JS Front End/JS Syntax Fundamentals - Lab/sumFirstAndLastArrayElements.js
@@ -0,0 +1,10 @@
+function sumFirstAndLastArrayElements(arr) {
+    let firstElement = arr[0];
+    let lastElement = arr[arr.length - 1];
+
+    console.log(firstElement + lastElement);
+}
+
+sumFirstAndLastArrayElements([20, 30, 40]);
+sumFirstAndLastArrayElements([10, 17, 22, 33]);
+sumFirstAndLastArrayElements([11, 58, 69]);
