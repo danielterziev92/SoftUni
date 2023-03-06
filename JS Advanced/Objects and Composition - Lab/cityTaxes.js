84328c2ccef46efa73bc184ac2beeea9b501637d
danielterziev92 <daniel.st.terziev@gmail.com>
2023-03-06 08:12:32 +0200

    City Taxes

---
+++ JS Advanced/Objects and Composition - Lab/cityTaxes.js
@@ -0,0 +1,28 @@
+function cityTaxes(name, population, treasury) {
+    const city = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += Math.floor(this.population * this.taxRate)
+        },
+        applyGrowth(percentage) {
+            this.population += Math.floor((this.population / 100) * percentage)
+        },
+        applyRecession(percentage) {
+            this.treasury -= Math.floor((this.treasury / 100) * percentage)
+        }
+    }
+
+    return city;
+}
+
+const city =
+    cityTaxes('Tortuga',
+        7000,
+        15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
