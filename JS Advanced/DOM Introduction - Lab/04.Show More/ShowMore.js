f94c826256aec4101c9f8d197de97f7b9423d976
danielterziev92 <105807682+danielterziev92@users.noreply.github.com>
2023-03-12 17:46:37 +0200

    Create ShowMore.js

---
+++ JS Advanced/DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const linkText = document.getElementById('more');
+    linkText.style.display = 'none';
+    const spanText = document.getElementById('text');
+    spanText.style.display = 'inline';
+}
