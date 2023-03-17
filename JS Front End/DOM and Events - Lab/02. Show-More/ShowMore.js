1162bd12df38adfd31f6e65d3cda3e6cc2a9268e
danielterziev92 <105807682+danielterziev92@users.noreply.github.com>
2023-03-17 23:25:38 +0200

    Create ShowMore.js

---
+++ JS Front End/DOM and Events - Lab/02. Show-More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const button = document.getElementById('more');
+    const text = document.getElementById('text');
+    button.style.display = 'none';
+    text.style.display = 'inline';
+}
