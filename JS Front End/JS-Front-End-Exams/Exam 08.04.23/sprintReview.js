4c33f2af11af39e17ff1cea10d13a163f2193155
danielterziev92 <105807682+danielterziev92@users.noreply.github.com>
2023-04-18 17:51:52 +0300

    Update sprintReview.js

--- JS Front End/JS-Front-End-Exams/Exam 08.04.23/sprintReview.js
+++ JS Front End/JS-Front-End-Exams/Exam 08.04.23/sprintReview.js
@@ -1,119 +1,117 @@
-function solve(params) {
-    let [n, ...leftParams] = params
+function solution(input) {
+    const [numb, ...data] = input
     const assignees = {}
 
-    for (let i = 0; i < n; i++) {
-        const info = leftParams.shift();
-        const [assignee, taskId, title, status, estimatedPoints] = info.split(':')
-        if (!assignees.hasOwnProperty(assignee)) {
-            assignees[assignee] = []
+    for (let i = 0; i < Number(numb); i++) {
+        const assigneeInfo = data.shift();
+        const [name, taskId, title, status, points] = assigneeInfo.split(':');
+        if (!assignees.hasOwnProperty(name)) {
+            assignees[name] = []
         }
-        assignees[assignee].push({taskId, title, status, estimatedPoints});
+
+        addAssignee(name, taskId, title, status, points);
     }
 
-    for (const command of leftParams) {
-        const [currentCommand, ...newParams] = command.split(':');
-        if (currentCommand === 'Add New') {
-            const [assignee, taskId, title, status, estimatedPoints] = newParams;
+    for (const commands of data) {
+        const [command, assignee, ...params] = commands.split(':')
 
-            if (!assignees.hasOwnProperty(assignee)) {
-                console.log(`Assignee ${assignee} does not exist on the board!`);
-                continue;
+        if (!assignees.hasOwnProperty(assignee)) {
+            console.log(`Assignee ${assignee} does not exist on the board!`);
+            continue;
+        }
+
+        if (command === 'Add New') {
+            const [taskId, title, status, points] = params;
+
+            addAssignee(assignee, taskId, title, status, points);
+        } else if (command === 'Change Status') {
+            const [taskId, newStatus] = params;
+
+
+            let isExist = false;
+            for (const task of assignees[assignee]) {
+                if (task.taskId === taskId) {
+                    task.status = newStatus;
+                    isExist = true;
+                }
             }
 
-            assignees[assignee].push({taskId, title, status, estimatedPoints});
-
-        } else if (currentCommand === 'Change Status') {
-            const [assignee, taskId, newStatus] = newParams;
-            if (!assignees.hasOwnProperty(assignee)) {
-                console.log(`Assignee ${assignee} does not exist on the board!`);
-                continue;
-            }
-
-            if (assignees[assignee].taskId !== taskId) {
+            if (!isExist) {
                 console.log(`Task with ID ${taskId} does not exist for ${assignee}!`);
-                continue;
             }
 
-            assignees[assignees][taskId].status = newStatus;
-        } else if (currentCommand === 'Remove Task') {
-            let [assignee, index] = newParams;
-            index = Number(index)
-            const maxIndex = Object.keys(assignees).length;
+        } else if (command === 'Remove Task') {
+            let [index] = params;
+            index = Number(index);
 
-            if (index >= maxIndex) {
+            if (index >= assignees[assignee].length || index < 0) {
                 console.log('Index is out of range!');
                 continue;
             }
 
-            if (!assignees.hasOwnProperty(assignee)) {
-                console.log(`Assignee ${assignee} does not exist on the board!`);
-                continue;
-            }
-
-            delete assignees[assignee];
+            assignees[assignee].splice(index, 1);
         }
     }
-    const statusPoints = {}
+
+    const tasksPoints = {
+        'ToDo': 0,
+        'In Progress': 0,
+        'Code Review': 0,
+        'Done': 0,
+    }
 
     for (const assignee in assignees) {
         for (const task of assignees[assignee]) {
-            const {status, estimatedPoints} = task;
-            if (!statusPoints.hasOwnProperty(status)) {
-                statusPoints[status] = 0;
-            }
-
-            statusPoints[status] += Number(estimatedPoints);
+            tasksPoints[task.status] += Number(task.points);
         }
     }
 
-    Object.entries(statusPoints).map(currentStatus => {
-        if (currentStatus[0] === 'Done') {
-            currentStatus[0] = 'Done Points';
-        }
-        console.log(`${currentStatus[0]}: ${currentStatus[1]}pts`)
-    })
+    let donePoints = 0;
+    let otherPoints = 0;
 
-    const combinedTasks = ["ToDo", "In Progress", "Code Review"]
-    let sumCombinedTasks = 0;
-    let sumDoneTasks = 0;
-
-    Object.entries(statusPoints).map(currentStatus => {
-        const [status, points] = currentStatus;
-        if (status !== 'Done') {
-            sumCombinedTasks += points;
+    Object.entries(tasksPoints).map(status => {
+        let [name, points] = status;
+        if (name === 'Done') {
+            name += ' Points';
+            donePoints += points;
         } else {
-            sumDoneTasks += points;
+            otherPoints += points;
         }
-    })
 
-    sumDoneTasks >= sumCombinedTasks
-        ? console.log("Sprint was successful!")
-        : console.log("Sprint was unsuccessful...")
+        console.log(`${name}: ${points}pts`)
+    });
+
+    donePoints >= otherPoints
+        ? console.log('Sprint was successful!')
+        : console.log('Sprint was unsuccessful...')
+
+    function addAssignee(name, taskId, title, status, points) {
+        assignees[name].push({taskId, title, status, points});
+    }
 }
 
-solve([
-    '5',
-    'Kiril:BOP-1209:Fix Minor Bug:ToDo:3',
-    'Mariya:BOP-1210:Fix Major Bug:In Progress:3',
-    'Peter:BOP-1211:POC:Code Review:5',
-    'Georgi:BOP-1212:Investigation Task:Done:2',
-    'Mariya:BOP-1213:New Account Page:In Progress:13',
-    'Add New:Kiril:BOP-1217:Add Info Page:In Progress:5',
-    'Change Status:Peter:BOP-1290:ToDo',
-    'Remove Task:Mariya:1',
-    'Remove Task:Joro:1',
-]);
-
-// solve([
-//     '4',
-//     'Kiril:BOP-1213:Fix Typo:Done:1',
-//     'Peter:BOP-1214:New Products Page:In Progress:2',
-//     'Mariya:BOP-1215:Setup Routing:ToDo:8',
-//     'Georgi:BOP-1216:Add Business Card:Code Review:3',
-//     'Add New:Sam:BOP-1237:Testing Home Page:Done:3',
-//     'Change Status:Georgi:BOP-1216:Done',
-//     'Change Status:Will:BOP-1212:In Progress',
-//     'Remove Task:Georgi:3',
-//     'Change Status:Mariya:BOP-1215:Done',
+// solution([
+//     '5',
+//     'Kiril:BOP-1209:Fix Minor Bug:ToDo:3',
+//     'Mariya:BOP-1210:Fix Major Bug:In Progress:3',
+//     'Peter:BOP-1211:POC:Code Review:5',
+//     'Georgi:BOP-1212:Investigation Task:Done:2',
+//     'Mariya:BOP-1213:New Account Page:In Progress:13',
+//     'Add New:Kiril:BOP-1217:Add Info Page:In Progress:5',
+//     'Change Status:Peter:BOP-1290:ToDo',
+//     'Remove Task:Mariya:1',
+//     'Remove Task:Joro:1',
 // ]);
+
+solution([
+    '4',
+    'Kiril:BOP-1213:Fix Typo:Done:1',
+    'Peter:BOP-1214:New Products Page:In Progress:2',
+    'Mariya:BOP-1215:Setup Routing:ToDo:8',
+    'Georgi:BOP-1216:Add Business Card:Code Review:3',
+    'Add New:Sam:BOP-1237:Testing Home Page:Done:3',
+    'Change Status:Georgi:BOP-1216:Done',
+    'Change Status:Will:BOP-1212:In Progress',
+    'Remove Task:Georgi:3',
+    'Change Status:Mariya:BOP-1215:Done',
+]);
