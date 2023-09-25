4da3f0f23602ad1bd432642a45327cd93eddcea7
danielterziev92 <daniel.st.terziev@gmail.com>
2023-09-26 00:10:40 +0300

    Add task for built-in functions - Exercise: 03. Capital Code

---
+++ PostgreSQL/Built-in Functions - Exercise/03. Capital Code.sql
@@ -0,0 +1,9 @@
+console_1ALTER TABLE countries
+    ADD COLUMN capital_code char(2);
+
+UPDATE countries
+SET capital_code = substring(capital, 1, 2)
+WHERE capital_code is null;
+
+SELECT *
+FROM countries;
