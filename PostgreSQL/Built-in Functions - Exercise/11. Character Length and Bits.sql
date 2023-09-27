6d14c0f2bd267da625574973e5f4bda6c4b6be6d
danielterziev92 <daniel.st.terziev@gmail.com>
2023-09-27 14:47:10 +0300

    Add task for build-in functions - Exercise: 11. Character Length and Bits

---
+++ PostgreSQL/Built-in Functions - Exercise/11. Character Length and Bits.sql
@@ -0,0 +1,6 @@
+console_1SELECT "Mountain Information",
+       length("Mountain Information")     as "Characters Length",
+       bit_length("Mountain Information") as "Bits of a String"
+FROM (SELECT concat_ws(' ', m.mountain_range, p.peak_name) as "Mountain Information"
+      FROM mountains as m
+               JOIN peaks as p ON m.id = p.mountain_id) as q;
