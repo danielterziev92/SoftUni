9913f457ce933c8265cca06fa64eef59a0197d5d
danielterziev92 <daniel.st.terziev@gmail.com>
2023-09-30 16:48:48 +0300

    Add task for subqueries and joins - Exercice: 07. Booking made by lead

---
+++ PostgreSQL/Subqueries and JOINs - Exercise/07. Bookings Made by Lead.sql
@@ -0,0 +1,8 @@
+SELECT b.apartment_id,
+       b.booked_for,
+       c.first_name,
+       c.country
+FROM bookings as b
+         JOIN customers as c
+              USING (customer_id)
+WHERE c.job_type LIKE '%Lead%';
