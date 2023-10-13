4b111f31f28ca69eaa2d93702f4ceb47055c57fa
danielterziev92 <daniel.st.terziev@gmail.com>
2023-10-13 14:42:24 +0300

    Add task for functions and procedures - Exercise: 03. User-defined function is word comprised

---
+++ PostgreSQL/Functions and Procedures - Exercise/03. User-defined Function Is Word Comprised.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION fn_is_word_comprised(set_of_letter VARCHAR(50), word VARCHAR(50))
+    RETURNS BOOLEAN AS
+$$
+DECLARE
+    word_length INT;
+    i           INT := 1;
+
+BEGIN
+    word_length := length(word);
+
+    WHILE i <= length(set_of_letter)
+        LOOP
+            IF position(substring(set_of_letter, i, 1) in word) > 0 THEN
+                word_length := word_length - 1;
+                IF word_length = 0 THEN
+                    RETURN true;
+                END IF;
+            END IF;
+            i := i + 1;
+        END LOOP;
+    RETURN false;
+END;
+$$
+    LANGUAGE plpgsql;
