8d4836b10fd1d17e520b5fd0a4e2fd8fb64c5667
danielterziev92 <daniel.st.terziev@gmail.com>
2023-10-29 17:07:31 +0200

    Create movies array with date

---
+++ ReactJS/JSX/Rent4u/src/assets/movies.js
@@ -0,0 +1,22 @@
+const movies = [
+    {
+        movie: 'Black panther',
+        directedBy: 'Ryan Coogler',
+        releaseDates: new Date('2018-01-29'),
+        runningTime: 134
+    },
+    {
+        movie: 'Doctor Strange in the Multiverse of Madness',
+        directedBy: 'Sam Raimi',
+        releaseDates: new Date('2022-05-02'),
+        runningTime: 126
+    },
+    {
+        movie: 'The Avengers',
+        directedBy: 'Joss Whedon',
+        releaseDates: new Date('2012-04-11'),
+        runningTime: 143
+    },
+]
+
+export default movies;
