e631968f575a06626c67aedd8e4fea9c7c5a54e6
danielterziev92 <daniel.st.terziev@gmail.com>
2023-10-29 17:06:01 +0200

    Add exercise for props to child element

--- ReactJS/JSX/Rent4u/src/App.jsx
+++ ReactJS/JSX/Rent4u/src/App.jsx
@@ -1,8 +1,12 @@
+import Header from "./components/Header.jsx";
+import Navigation from "./components/Navigation.jsx";
+
 function App() {
 
     return (
         <>
-
+            <Header isChecked={true}/>
+            <Navigation/>
         </>
     )
 }
