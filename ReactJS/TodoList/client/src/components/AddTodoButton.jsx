2a43e2636eac348b871837a07a6cbbb96730b930
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-01 20:56:47 +0200

    Add AddTodoButton component to add taks in todo list

---
+++ ReactJS/TodoList/client/src/components/AddTodoButton.jsx
@@ -0,0 +1,7 @@
+export default function AddTodoButton() {
+    return (
+        <div className="add-btn-container">
+            <button className="btn">+ Add new Todo</button>
+        </div>
+    )
+}
