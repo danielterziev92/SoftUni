c7f55f1be4425d94fb411553a01dda40a854a47e
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-01 21:01:23 +0200

    Add functionality to change the status of taks in todo list

--- ReactJS/TodoList/client/src/components/TodoItem.jsx
+++ ReactJS/TodoList/client/src/components/TodoItem.jsx
@@ -1,10 +1,36 @@
-export default function TotoItem({task, status, action}) {
+export default function TotoItem({id, task, status}) {
+
+    const changeStatus = async () => {
+
+        try {
+
+            const url = 'http://localhost:3030/jsonstore/todos'
+
+            const response = await fetch(`${url}/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({_id: id, text: task, isCompleted: !status})
+            });
+
+            if (response.ok) {
+                console.log('Data update successfully');
+            } else {
+                console.error('Update data failed');
+            }
+
+        } catch (e) {
+            console.log("Fetch error:", e)
+        }
+    }
+
     return (
-        <tr className="todo is-completed">
-            <td>Give dog a bath</td>
-            <td>Complete</td>
+        <tr className={`todo  ${status ? "is-completed" : ""}`}>
+            <td>{task}</td>
+            <td>{status ? "Complete" : "Incomplete"}</td>
             <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
+                <button className="btn todo-btn" onClick={changeStatus}>Change status</button>
             </td>
         </tr>
     );
