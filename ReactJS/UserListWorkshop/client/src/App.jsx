0d877303ae90e5d359c343338941f5b0bbca4a7d
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-03 19:48:40 +0200

    Add Footer component to the project

--- ReactJS/UserListWorkshop/client/src/App.jsx
+++ ReactJS/UserListWorkshop/client/src/App.jsx
@@ -1,11 +1,14 @@
 import {Header} from "./components/Header.jsx";
 import './styles.css'
+import {Footer} from "./components/Footer.jsx";
 
 function App() {
 
     return (
         <div>
             <Header/>
+
+            <Footer/>
         </div>
     )
 }
---
+++ ReactJS/UserListWorkshop/client/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>This site is designed to be used for training purposes at SoftUni.</p>
+        </footer>
+    );
+};
