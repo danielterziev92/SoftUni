dacd553a19c74bb182216b776393cac97e290534
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-03 20:05:28 +0200

    Add fontawesome in style.css and crate SearchForm component for the project

--- ReactJS/UserListWorkshop/client/src/App.jsx
+++ ReactJS/UserListWorkshop/client/src/App.jsx
@@ -1,13 +1,18 @@
 import {Header} from "./components/Header.jsx";
 import './styles.css'
 import {Footer} from "./components/Footer.jsx";
+import {SearchForm} from "./components/SearchForm.jsx";
 
 function App() {
 
     return (
         <div>
             <Header/>
-
+            <main className="main">
+                <section className="card users-container">
+                    <SearchForm/>
+                </section>
+            </main>
             <Footer/>
         </div>
     )
