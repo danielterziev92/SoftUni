cda9e33e7d76c0c0081f521b187225a4e0dd5d7a
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-04 17:39:12 +0200

    Add clickSearchFormHandler to get all users, filter the by some criteria and pass them to the componet

--- ReactJS/UserListWorkshop/client/src/App.jsx
+++ ReactJS/UserListWorkshop/client/src/App.jsx
@@ -2,19 +2,34 @@ import {Header} from "./components/Header.jsx";
 import './styles.css'
 import {Footer} from "./components/Footer.jsx";
 import {SearchForm} from "./components/SearchForm.jsx";
+import {TableWithData} from "./components/TableWithData.jsx";
+import {useEffect, useState} from "react";
+import {filteredData} from "./services/sortData.js";
+import {getAllUsers} from "./services/userService.js";
 
 function App() {
+    const [initialUsers, setInitialUsers] = useState([]);
+
+    const clickSearchFormHandler = async (searchedValue, searchedCriteria) => {
+        const response = await getAllUsers()
+        const filteredUsers = filteredData(response, searchedValue, searchedCriteria);
+        console.log(`filtered: ${filteredUsers}`)
+        setInitialUsers(filteredUsers);
+    }
 
     return (
-        <div>
+        <>
             <Header/>
             <main className="main">
                 <section className="card users-container">
-                    <SearchForm/>
+                    <SearchForm clickSearchFormHandler={clickSearchFormHandler}/>
+                    <div className="table-wrapper">
+                        <TableWithData initialUsers={initialUsers}/>
+                    </div>
                 </section>
             </main>
             <Footer/>
-        </div>
+        </>
     )
 }
 
