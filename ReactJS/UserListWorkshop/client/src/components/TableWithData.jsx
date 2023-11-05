96fa63fce8ffe2a3a33d2ab64e96a5252fd06b03
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-05 18:28:27 +0200

    Add function to prepare data from entries and return formatted data. Add function to update user. Create funtion to fetch user details, update it to setUsers and pre-render all users component

--- ReactJS/UserListWorkshop/client/src/components/TableWithData.jsx
+++ ReactJS/UserListWorkshop/client/src/components/TableWithData.jsx
@@ -2,33 +2,46 @@ import {TableWithDataItem} from "./TableWithDataItem.jsx";
 import {useEffect, useState} from "react";
 import {ErrorOverlap} from "./overlaps/ErrorOverlap.jsx";
 import {NoUsersOverlap} from "./overlaps/NoUsersOverlap.jsx";
-import {getAllUsers} from "../services/userService.js";
+import {createUser, getAllUsers, getUser, updateUser} from "../services/userService.js";
 import {TableWithDataHead} from "./TableWithDataHead.jsx";
 import {
+    filteredData,
     sortDataByCreatedAt,
     sortDataByEmail,
     sortDataByFirstName,
     sortDataByLastName,
     sortDataByPhone
 } from "../services/sortData.js";
+import {Spinner} from "./Spinner.jsx";
+import {UserInfoModal} from "./UserInfoModal.jsx";
+import {UserCreateModal} from "./UserCreateModal.jsx";
+import {UserUpdateModal} from "./UserUpdateModal.jsx";
+import {UserDeleteModal} from "./UserDeleteModal.jsx";
 
 export const TableWithData = ({
-                                  initialUsers,
+                                  searchValue,
+                                  searchCriteria,
                               }) => {
     const [users, setUsers] = useState([]);
+    const [user, setUser] = useState({});
     const [isFetchFailed, setIsFetchFailed] = useState(false);
+    const [filteredUsers, setFilteredUsers] = useState([]);
+    const [isSpinnerShow, setIsSpinnerShow] = useState(true);
+    const [showUserInfoModal, setShowUserInfoModal] = useState(false);
+    const [showUserUpdateModal, setShowUserUpdateModal] = useState(false);
+    const [showUserDeleteModal, setShowUserDeleteModal] = useState(false);
+    const [showUserCreateModal, setShowUserCreateModal] = useState(false);
 
     useEffect(() => {
         getAllUsers()
             .then(data => setUsers(data))
-            .catch(e => setIsFetchFailed(true));
+            .catch(e => setIsFetchFailed(true))
+            .finally(() => setIsSpinnerShow(false));
     }, []);
 
     useEffect(() => {
-        console.log(`initial: ${initialUsers}`)
-        setUsers(initialUsers)
-        console.log(users)
-    }, [initialUsers]);
+        setFilteredUsers(filteredData(users, searchValue, searchCriteria))
+    }, [searchValue, searchCriteria]);
 
     const sortDataClickHandler = (criterion) => {
         const criteria = {
@@ -41,23 +54,117 @@ export const TableWithData = ({
         setUsers(criteria[criterion](users));
     }
 
+    const onClickShowUserInfoModalHandler = async (userId) => {
+        const data = await getUser(userId);
+        setUser(data);
+        setShowUserInfoModal(true);
+    }
+
+    const onClickShowUserCreateModalHandler = () => {
+        setShowUserCreateModal(true);
+    }
+
+    const onClickShowUserUpdateModalHandler = async (userId) => {
+        const data = await getUser(userId);
+        setUser(data);
+        setShowUserUpdateModal(true);
+    }
+
+    const onClickShowUserDeleteModalHandler = async (userId) => {
+        const data = await getUser(userId);
+        setUser(data);
+        setShowUserDeleteModal(true);
+    }
+
+    const clickCreateNewUserHandler = async (e) => {
+        e.preventDefault();
+        const formDate = new FormData(e.currentTarget);
+        const newUser = await createUser(Object.fromEntries(formDate));
+        setUsers(state => [...state, newUser]);
+        closeModal();
+    }
+
+    const clickUpdateUserInfoHandler = async (e) => {
+        e.preventDefault();
+        const formDate = new FormData(e.currentTarget);
+        const newUser = await updateUser(user._id, Object.fromEntries(formDate));
+        const newUsersArray = users.map(u => {
+            if (u._id === newUser._id) {
+                return newUser
+            }
+            return u;
+        });
+        setUsers(newUsersArray.slice());
+        closeModal();
+    }
+
+    const clickDeleteUserInfoHandler = async (e) => {
+        e.preventDefault()
+
+    }
+
+    const closeModal = () => {
+        if (showUserInfoModal) {
+            return setShowUserInfoModal(false);
+        }
+
+        if (showUserCreateModal) {
+            return setShowUserCreateModal(false);
+        }
+
+        if (showUserUpdateModal) {
+            return setShowUserUpdateModal(false);
+        }
+
+        if (showUserDeleteModal) {
+            return setShowUserDeleteModal(false);
+        }
+    }
 
     return (
         <>
-            {isFetchFailed && <ErrorOverlap/>}
-            {users.length === 0 && !isFetchFailed && < NoUsersOverlap/>}
+            {isSpinnerShow && <Spinner/>}
+            {isFetchFailed && !isSpinnerShow && <ErrorOverlap/>}
+            {users.length === 0 && !isSpinnerShow && !isFetchFailed && <NoUsersOverlap/>}
 
-            {!isFetchFailed && users.length > 0 &&
-                <table className="table">
-                    <TableWithDataHead sortDataClickHandler={sortDataClickHandler}/>
-                    <tbody>
-                    {users.map(user =>
-                        (<TableWithDataItem
-                                {...user}
-                                key={user._id}/>
-                        ))}
-                    </tbody>
-                </table>
+            {!isFetchFailed && users.length > 0 && !isSpinnerShow &&
+                <>
+                    {showUserInfoModal && <UserInfoModal {...user} closeModal={closeModal}/>}
+
+                    {showUserCreateModal && <UserCreateModal closeModal={closeModal}
+                                                             clickCreateNewUserHandler={clickCreateNewUserHandler}/>}
+
+                    {showUserUpdateModal && <UserUpdateModal userDetail={user}
+                                                             closeModal={closeModal}
+                                                             clickUpdateUserInfoHandler={clickUpdateUserInfoHandler}/>}
+
+                    {showUserDeleteModal && <UserDeleteModal closeModal={closeModal}
+                                                             clickDeleteUserInfoHandler={clickDeleteUserInfoHandler}/>}
+
+                    <table className="table">
+                        <TableWithDataHead sortDataClickHandler={sortDataClickHandler}/>
+                        <tbody>
+                        {filteredUsers.length !== 0
+                            ? filteredUsers.map(user => (
+                                <TableWithDataItem {...user}
+                                                   key={user._id}
+                                                   onClickShowUserInfoModalHandler={onClickShowUserInfoModalHandler}
+                                                   onClickShowUserUpdateModalHandler={onClickShowUserUpdateModalHandler}
+                                                   onClickShowUserDeleteModalHandler={onClickShowUserDeleteModalHandler}
+                                />
+                            ))
+                            : users.map(user => (
+                                <TableWithDataItem {...user}
+                                                   key={user._id}
+                                                   onClickShowUserInfoModalHandler={onClickShowUserInfoModalHandler}
+                                                   onClickShowUserUpdateModalHandler={onClickShowUserUpdateModalHandler}
+                                                   onClickShowUserDeleteModalHandle={onClickShowUserDeleteModalHandler}
+                                />
+                            ))}
+                        </tbody>
+                    </table>
+                    <button className="btn-add btn" onClick={onClickShowUserCreateModalHandler}>Add new user</button>
+                </>
             }
         </>
     );
--- ReactJS/UserListWorkshop/client/src/services/userService.js
+++ ReactJS/UserListWorkshop/client/src/services/userService.js
@@ -1,5 +1,23 @@
 const baseUrl = 'http://localhost:3030/jsonstore/users'
 
+function prepareBodyData(data, currentCreatedAt, currentUpdatedAt) {
+    return {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        imageUrl: data.imageUrl,
+        phoneNumber: data.phoneNumber,
+        createdAt: currentCreatedAt,
+        updatedAt: currentUpdatedAt,
+        address: {
+            country: data.country,
+            city: data.city,
+            street: data.street,
+            streetNumber: data.streetNumber
+        }
+    }
+}
+
 export const getAllUsers = async () => {
     const response = await fetch(baseUrl);
 
@@ -19,4 +37,28 @@ export const getUser = async (userId) => {
     }
 
     return await response.json()
+}
+
+export const createUser = async (data) => {
+    const bodyData = prepareBodyData(data, new Date().toISOString(), new Date().toISOString())
+    const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(bodyData),
+    }
+
+    const response = await fetch(`${baseUrl}`, requestOptions);
+    return response.json()
+}
+
+export const updateUser = async (userId, details) => {
+    const bodyData = prepareBodyData(details, details.createdAt, new Date().toISOString())
+    const requestOptions = {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(bodyData)
+    }
+
+    const response = await fetch(`${baseUrl}/${userId}`, requestOptions);
+    return response.json();
 }
