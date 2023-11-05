7f6eaa34370dc8b83792f3f4f18a7f2e84fa3721
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-05 12:24:51 +0200

    Remove funtion formatData and create new file for it.

--- ReactJS/UserListWorkshop/client/src/components/TableWithDataItem.jsx
+++ ReactJS/UserListWorkshop/client/src/components/TableWithDataItem.jsx
@@ -1,17 +1,19 @@
+import formatDate from "../services/formatDate.js";
+
 export const TableWithDataItem = ({
+                                      _id,
                                       firstName,
                                       lastName,
                                       email,
                                       imageUrl,
                                       phoneNumber,
                                       createdAt,
+                                      onClickShowUserInfoModalHandler,
                                   }) => {
 
-    function formatDate(date) {
-        const option = {year: 'numeric', month: 'long', day: 'numeric'};
-        return new Date(date).toLocaleDateString('en-US', option);
+    const clickUserInfoHandler = () => {
+        onClickShowUserInfoModalHandler(_id)
     }
-
 
     return (
         <tr>
@@ -43,7 +45,7 @@ export const TableWithDataItem = ({
                         </path>
                     </svg>
                 </button>
-                <button className="btn info-btn" title="Info">
+                <button className="btn info-btn" title="Info" onClick={clickUserInfoHandler}>
                     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="info"
                          className="svg-inline--fa fa-info" role="img" xmlns="http://www.w3.org/2000/svg"
                          viewBox="-150 0 512 612">
