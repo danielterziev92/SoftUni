93564fd8749e8e92e0309fa23b17ad99e6fd7d15
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-09 20:25:38 +0200

    Create model for Bardoce of product witch will be many to many field for every product

--- ReactJS/FinalProject/server/server/product/models.py
+++ ReactJS/FinalProject/server/server/product/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Barcode(models.Model):
+    code = models.CharField(
+        max_length=128,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.code
