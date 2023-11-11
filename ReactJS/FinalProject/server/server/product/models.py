3a502d591bbfff2031019b00de6064fd83a7cd68
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-11 23:02:06 +0200

    Add quantity to product and add parent category to group

--- ReactJS/FinalProject/server/server/product/models.py
+++ ReactJS/FinalProject/server/server/product/models.py
@@ -25,6 +25,14 @@ class Group(models.Model):
         blank=False,
     )
 
+    parent_category = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='subcategories'
+    )
+
     def __str__(self):
         return self.name
 
@@ -51,6 +59,11 @@ class Product(models.Model):
     price = models.DecimalField(
         max_digits=10,
         decimal_places=3,
+    )
+
+    quantity = models.PositiveIntegerField(
+        null=True,
+        blank=True,
     )
 
     is_active = models.BooleanField(
