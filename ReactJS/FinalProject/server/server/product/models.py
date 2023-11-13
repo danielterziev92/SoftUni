7dbdd035201bbb142b77c4125e8066cfc699cde6
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-13 22:53:59 +0200

    Add Group model to the project

--- ReactJS/FinalProject/server/server/product/models.py
+++ ReactJS/FinalProject/server/server/product/models.py
@@ -11,3 +11,27 @@ class MoreBarcodes(models.Model):
     def __str__(self):
         return self.code
 
+
+class Group(models.Model):
+    code = models.CharField(
+        max_length=10,
+        null=True,
+        blank=True,
+    )
+
+    name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+    )
+
+    parent_category = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='subcategories'
+    )
+
+    def __str__(self):
+        return self.name
