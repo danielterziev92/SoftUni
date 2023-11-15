5a80c6a7b52b5edff9c3ae6f5e747b91c7702ea6
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-15 19:53:18 +0200

    Create navigation tab and make style for it

--- ReactJS/FinalProject/client/src/components/product-list-item-detail/ProductListItemDetail.jsx
+++ ReactJS/FinalProject/client/src/components/product-list-item-detail/ProductListItemDetail.jsx
@@ -3,18 +3,13 @@ import {useEffect, useState} from "react";
 import Spinner from "../spinner/Spinner.jsx";
 import {getProductById} from "../../services/productService.js";
 import ProductDetailBaseInfo from "../product-detail-base-info/ProductDetailBaseInfo.jsx";
+import ProductListNavigationTabs from "../product-list-navigation-tabs/ProductListNavigationTabs.jsx";
 
 export default function ProductListItemDetail({id, setShowDetail}) {
 
     const [productData, setProductData] = useState({});
     const [isSpinnerShow, setIsSpinnerShow] = useState(true);
     const [activeTab, setActiveTab] = useState('base-info');
-
-    const changeActiveTabHandler = (e) => {
-        console.log(e.target.name)
-        setActiveTab(e.target.name);
-    }
-
 
     const closeShowDetailClickHandler = () => {
         setShowDetail(false);
@@ -34,16 +29,10 @@ export default function ProductListItemDetail({id, setShowDetail}) {
             {isSpinnerShow && <Spinner/>}
             {!isSpinnerShow &&
                 <>
-                    <div className={style.navigationTabs}>
-                        <ul>
-                            <li name="base-info" onClick={changeActiveTabHandler}>Основни данни</li>
-                            <li name="addit-info" onClick={changeActiveTabHandler}>Допълнителни данни</li>
-                            <li name="barcodes" onClick={changeActiveTabHandler}>Баркодове</li>
-                        </ul>
-                        <div className={style.closeButton} onClick={closeShowDetailClickHandler}>
-                            <i className="fa-solid fa-circle-xmark"></i>
-                        </div>
-                    </div>
+                    <ProductListNavigationTabs closeShowDetailClickHandler={closeShowDetailClickHandler}
+                                               setActiveTab={setActiveTab}
+                                               activeTab={activeTab}
+                    />
                     {activeTab === 'base-info' &&
                         <ProductDetailBaseInfo
                             id={id}
