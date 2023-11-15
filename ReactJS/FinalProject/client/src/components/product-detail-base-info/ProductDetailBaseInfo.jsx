edf79865f0f8f9840b721547e084a54e985e3549
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-15 22:47:57 +0200

    Make stylization for BaseInfo component and create get request to get all groups, then sort them if the have children

--- ReactJS/FinalProject/client/src/components/product-detail-base-info/ProductDetailBaseInfo.jsx
+++ ReactJS/FinalProject/client/src/components/product-detail-base-info/ProductDetailBaseInfo.jsx
@@ -1,5 +1,7 @@
-import { useState} from "react";
-import {patchProductById} from "../../services/productService.js";
+import {useEffect, useState} from "react";
+import {getAllGroups, patchProductById} from "../../services/productService.js";
+
+import style from './ProductDetailBaseInfo.module.css';
 
 export default function ProductDetailBaseInfo({
                                                   id,
@@ -10,12 +12,17 @@ export default function ProductDetailBaseInfo({
                                               }) {
 
     const [dataToChange, setDataToChange] = useState({});
+    const [groups, setGroups] = useState([]);
 
     const changeHandler = (e) => {
-        let {name, value} = e.target;
+        let {type, name, value} = e.target;
 
-        if (name === 'price' || name === 'quantity' || name === 'group') {
+        if (type === 'number') {
             value = Number(value);
+        }
+
+        if (type === 'checkbox') {
+            value = e.target.checked
         }
 
         setDataToChange(state => ({
@@ -26,50 +33,80 @@ export default function ProductDetailBaseInfo({
 
     const changeProductDataSubmitHandler = (e) => {
         e.preventDefault();
-        patchProductById(id, dataToChange).then(setProductData);
+        patchProductById(id, dataToChange).then(data => setProductData(data));
+
+        // closeShowDetailClickHandler();
     }
 
-    return (
-        <form onSubmit={changeProductDataSubmitHandler}>
-            <label htmlFor="name">
-                Име:
-                <input type="text" name="name"
-                       value={dataToChange.name || productData.name}
-                       onChange={changeHandler}/>
-            </label>
-            <label htmlFor="code">
-                Код:
-                <input type="text" name="code"
-                       value={dataToChange.code || productData.code}
-                       onChange={changeHandler}/>
-            </label>
-            <label htmlFor="barcode">
-                Баркод:
-                <input type="text" name="name"
-                       value={dataToChange.barcode || productData.barcode}
-                       onChange={changeHandler}/>
-            </label>
-            <label htmlFor="price">
-                Цена:
-                <input type="number" step="0.01" name="price"
-                       value={dataToChange.price || productData.price}
-                       onChange={changeHandler}/>
-            </label>
-            <label htmlFor="quantity">
-                Количество:
-                <input type="number" step="1" name="quantity"
-                       value={dataToChange.quantity || productData.quantity}
-                       onChange={changeHandler}/>
-            </label>
-            <label htmlFor="is_active">
-                Активен:
-                <input type="checkbox" name="is_active"
-                       checked={dataToChange.is_active || productData.is_active}
-                       onChange={changeHandler}/>
-            </label>
+    const sortGroup = (data) => {
+        const grouped = {};
+        const result = [];
 
-            <button>Промени</button>
-            <button type="button" onClick={closeShowDetailClickHandler}>Отказ</button>
+        data.forEach(item => {
+            const {parent_category, ...rest} = item;
+
+            if (!parent_category) {
+                result.push({...rest, children: []});
+            } else {
+                grouped[parent_category] = grouped[parent_category] || [];
+                grouped[parent_category].push({...rest, children: []});
+            }
+        });
+
+        result.forEach(parent => {
+            parent.children = grouped[parent.id] || [];
+        });
+
+        return result;
+    };
+
+    useEffect(() => {
+        console.log(groups)
+    }, [groups]);
+
+    useEffect(() => {
+        setDataToChange(productData)
+        getAllGroups().then(data => setGroups(sortGroup(data)));
+    }, []);
+
+    return (
+        <form onSubmit={changeProductDataSubmitHandler} className={style.baseInfoForm}>
+            <div className={style.name}>
+                <label htmlFor="name">Име:</label>
+                <input id="name" type="text" name="name" value={dataToChange.name} onChange={changeHandler}/>
+            </div>
+            <div className={style.code}>
+                <label htmlFor="code">Код:</label>
+                <input id="code" type="text" name="code" value={dataToChange.code} onChange={changeHandler}/>
+            </div>
+            <div className={style.barcode}>
+                <label htmlFor="barcode">Баркод:</label>
+                <input id="barcode" type="text" name="name" value={dataToChange.barcode} onChange={changeHandler}/>
+            </div>
+            <div className={style.price}>
+                <label htmlFor="price">Цена:</label>
+                <input id="price" type="number" step="0.01" name="price" value={dataToChange.price}
+                       onChange={changeHandler}/>
+            </div>
+            <div className={style.quantity}>
+                <label htmlFor="quantity">Количество:</label>
+                <input id="quantity" type="number" step="1" name="quantity" value={dataToChange.quantity}
+                       onChange={changeHandler}/>
+            </div>
+            <div className={style.active}>
+                <label htmlFor="is_active">Активен:</label>
+                <input id="is_active" type="checkbox" name="is_active" checked={dataToChange.is_active}
+                       onChange={changeHandler}/>
+            </div>
+            <div className={style.groups}>
+
+            </div>
+            <div className={style.buttons}>
+                <button>Нулирай</button>
+                <button>Изтрий</button>
+                <button>Промени</button>
+                <button type="button" onClick={closeShowDetailClickHandler}>Отказ</button>
+            </div>
         </form>
     );
 }
--- ReactJS/FinalProject/client/src/services/productService.js
+++ ReactJS/FinalProject/client/src/services/productService.js
@@ -3,14 +3,18 @@ const baseUrl = 'http://localhost:8000/api'
 import * as request from '../lib/request.js'
 
 export const getAllProducts = async () => {
-    return request.get(`${baseUrl}/products`);
+    return request.get(`${baseUrl}/products/`);
 }
 
 export const getProductById = async (productId) => {
     // await new Promise(resolve => setTimeout(resolve, 5000));
-    return request.get(`${baseUrl}/product/${productId}`);
+    return request.get(`${baseUrl}/product/${productId}/`);
 }
 
 export const patchProductById = async (productId, data) => {
-    return request.patch(`${baseUrl}/product/${productId}/`, data);
+    return request.put(`${baseUrl}/product/${productId}/`, data);
+}
+
+export const getAllGroups = async () => {
+    return request.get(`${baseUrl}/groups/`)
 }
