06b7a2659df75d6bbacda317894b626ef8f3dc20
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-18 22:21:40 +0200

    Create functions to update state of message state in App component

--- ReactJS/FinalProject/client/src/App.jsx
+++ ReactJS/FinalProject/client/src/App.jsx
@@ -5,10 +5,17 @@ import Products from "./components/products/Products.jsx";
 
 import mainStyle from "./components/Main.module.css";
 import {MessageContext} from "./contexts/MessageContext.js";
+import {useState} from "react";
 
 export default function App() {
+    const [message, setMessage] = useState({message: '', status: ''})
+
+    const updateMessage = (newMessage) => setMessage(state => ({...state, message: newMessage}));
+
+    const updateStatus = (newStatus) => setMessage(state => ({...state, status: newStatus}));
+
     return (
-        <MessageContext.Provider value={{message: '', status: ''}}>
+        <MessageContext.Provider value={{message, updateMessage, updateStatus}}>
             <Aside/>
             <main className={mainStyle.Main}>
                 <Routes>
