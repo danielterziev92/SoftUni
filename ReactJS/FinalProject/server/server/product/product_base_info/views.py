8d732a5003ca85a299c885116f05d33f0c2adc89
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-24 22:36:47 +0200

    Create ProductValidationMixin and use it in create and update views

--- ReactJS/FinalProject/server/server/product/product_base_info/views.py
+++ ReactJS/FinalProject/server/server/product/product_base_info/views.py
@@ -4,29 +4,12 @@ from server.product.models import ProductBaseInformation
 from server.product.product_base_info.serializers import ProductListSerializer, ProductSerializer
 
 
-class ProductListView(rest_views.ListAPIView):
-    queryset = ProductBaseInformation.objects.all()
-    serializer_class = ProductListSerializer
-
-
-class ProductDetailView(rest_views.RetrieveUpdateDestroyAPIView):
-    queryset = ProductBaseInformation.objects.all()
-    serializer_class = ProductSerializer
-
-
-class ProductCreateView(rest_views.CreateAPIView):
-    queryset = ProductBaseInformation.objects.all()
-    serializer_class = ProductSerializer
-
-    def create(self, request, *args, **kwargs):
-        self.check_all_fields()
-
-        return super().create(request, *args, **kwargs)
-
+class ProductValidationMixin:
     def check_all_fields(self):
         serializer = self.get_serializer(data=self.request.data)
 
         fields_to_check = {
+            'name': serializer.initial_data.get('name'),
             'code': serializer.initial_data.get('code'),
             'barcode': serializer.initial_data.get('barcode'),
             'quantity': serializer.initial_data.get('quantity'),
@@ -56,3 +39,34 @@ class ProductCreateView(rest_views.CreateAPIView):
             if value == 0:
                 error_message = {'message': error_messages[key]}
                 raise serializers.ValidationError(error_message)
+
+        if not value:
+            error_message = {'message': 'Всички полета са задължителни'}
+            raise serializers.ValidationError(error_message)
+
+
+class ProductListView(rest_views.ListAPIView):
+    queryset = ProductBaseInformation.objects.all()
+    serializer_class = ProductListSerializer
+
+
+class ProductDetailView(ProductValidationMixin, rest_views.RetrieveUpdateDestroyAPIView):
+    queryset = ProductBaseInformation.objects.all()
+    serializer_class = ProductSerializer
+
+    def put(self, request, *args, **kwargs):
+        self.check_all_fields()
+        return super().put(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        self.check_all_fields()
+        return super().patch(request, *args, **kwargs)
+
+
+class ProductCreateView(ProductValidationMixin, rest_views.CreateAPIView):
+    queryset = ProductBaseInformation.objects.all()
+    serializer_class = ProductSerializer
+
+    def create(self, request, *args, **kwargs):
+        self.check_all_fields()
+        return super().create(request, *args, **kwargs)
