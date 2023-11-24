c7dcf9e1bae2d888365e8bf69a18942f8a1f0f47
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-24 21:16:33 +0200

    Add function to check some type and return custom error message

--- ReactJS/FinalProject/server/server/product/product_base_info/views.py
+++ ReactJS/FinalProject/server/server/product/product_base_info/views.py
@@ -19,21 +19,40 @@ class ProductCreateView(rest_views.CreateAPIView):
     serializer_class = ProductSerializer
 
     def create(self, request, *args, **kwargs):
-        self.check_is_already_exist()
+        self.check_all_fields()
 
         return super().create(request, *args, **kwargs)
 
-    def check_is_already_exist(self):
-        message_field_type = {
-            'code': 'код',
-            'barcode': 'баркод',
+    def check_all_fields(self):
+        serializer = self.get_serializer(data=self.request.data)
+
+        fields_to_check = {
+            'code': serializer.initial_data.get('code'),
+            'barcode': serializer.initial_data.get('barcode'),
+            'quantity': serializer.initial_data.get('quantity'),
+            'price': serializer.initial_data.get('price'),
+            'group': serializer.initial_data.get('group'),
         }
 
-        serializer = self.get_serializer(data=self.request.data)
-        serializer.is_valid(raise_exception=True)
+        for field in fields_to_check.keys():
+            self.check_is_already_exist(field, fields_to_check[field])
 
-        for field in message_field_type.keys():
-            value = serializer.validated_data.get(field)
-            if ProductBaseInformation.objects.filter(**{field: value}).exists():
-                error_message = {'message': f'Продукт с {message_field_type[field]}: {value} вече съществува.'}
+    @staticmethod
+    def check_is_already_exist(key, value):
+        error_messages = {
+            'code': f'Продукт с този код: {value} вече съществува.',
+            'barcode': f'Продукт с този баркод: {value} вече съществува.',
+            'quantity': f'Количеството не може да бъде {value}',
+            'price': f'Цената не може да бъде {value}',
+            'group': f'Трабва да изберете група',
+        }
+
+        if type(value) == str:
+            if ProductBaseInformation.objects.filter(**{key: value}).exists():
+                error_message = {'message': error_messages[key]}
+                raise serializers.ValidationError(error_message)
+
+        if type(value) == int:
+            if value == 0:
+                error_message = {'message': error_messages[key]}
                 raise serializers.ValidationError(error_message)
