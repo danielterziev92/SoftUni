320512a5e801dcec011d029d5c1c34dc4d80315b
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-25 23:54:56 +0200

    Finish with style of Login Component

--- ReactJS/FinalProject/client/src/components/login/Login.jsx
+++ ReactJS/FinalProject/client/src/components/login/Login.jsx
@@ -1,47 +1,55 @@
 import {useContext, useEffect, useRef} from "react";
+import {Link} from "react-router-dom";
 
-import loginStyle from './Login.module.css';
+import loginStyle from '../index/Index.module.css';
 
 import useForm from "../../hooks/useForm.js";
-
 import {FormContext} from "../../contexts/FormContext.js";
+import Paths from "../../utils/Paths.js";
 
 const initialUserData = {
     username: '',
     password: '',
 }
 
-const FormKeyTranslation = {
-    username: 'Потребителско име',
-    password: 'Парола',
+const FormInformation = {
+    username: {type: 'text', label: 'Потребителско име'},
+    password: {type: 'password', label: 'Парола'},
 }
 
 export default function Login() {
     const focusedInput = useRef('username');
     const {formRef} = useContext(FormContext);
-    const {formValue, updateFormValue, changeDataHandler, onSubmitForm,} = useForm(initialUserData);
+    const {formValue, changeDataHandler, onSubmitForm,} = useForm(initialUserData);
 
     useEffect(() => {
-        focusedInput.current.focus();
+        if (focusedInput.current) {
+            focusedInput.current.focus();
+        }
     }, [focusedInput]);
 
 
-    // <div className={formStyle.name}>
-    //     <label htmlFor={FormKey.Name}>Име:</label>
-    //     <input id={FormKey.Name} type="text" name={FormKey.Name} value={formValue[FormKey.Name]}
-    //            onChange={inputChangeHandler}/>
-    // </div>
+    // <label htmlFor={FormKey.Name}>Име:</label>
+    // <input id={FormKey.Name} type="text" name={FormKey.Name} value={formValue[FormKey.Name]}
+    //        onChange={inputChangeHandler}/>
     return (
         <article>
             <form ref={formRef} onSubmit={onSubmitForm} className={loginStyle.Form}>
-                {Object.entries(initialUserData).map(([key, value]) => (
+                {Object.entries(formValue).map(([key, value]) => (
                     <div key={key}>
-                        <label htmlFor={key}>{FormKeyTranslation[key]}</label>
-                        <input id={key} value={value} name={formValue[key]} onChange={changeDataHandler}
-                               ref={focusedInput.current === key ? focusedInput : null}/>
+                        <label htmlFor={key}>{FormInformation[key].label}</label>
+                        <input id={key} value={formValue[key]} name={key} type={FormInformation[key].type}
+                               onChange={changeDataHandler}
+                               ref={focusedInput.current === key ? focusedInput : null}
+                        />
                     </div>
                 ))}
-                <button>Вход</button>
+                <div>
+                    <button>Вход</button>
+                </div>
+                <div>
+                    <Link to={Paths.register}>Регистрация</Link>
+                </div>
             </form>
         </article>
     );
---
+++ ReactJS/FinalProject/client/src/utils/Paths.js
@@ -0,0 +1,9 @@
+const Paths = {
+    index: '',
+    login: '/login',
+    register: '/register',
+    products: '/products',
+    groups: '/groups',
+}
+
+export default Paths;
