00096da37f709c21f1f0d97ce4b6cf488a314db9
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-27 23:50:21 +0200

    Add more information about user

--- ReactJS/FinalProject/server/server/user_app/serializers.py
+++ ReactJS/FinalProject/server/server/user_app/serializers.py
@@ -8,5 +8,9 @@ class AppTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         token['username'] = user.username
         token['email'] = user.email
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['is_staff'] = user.is_staff
+        token['is_superuser'] = user.is_superuser
 
         return token
