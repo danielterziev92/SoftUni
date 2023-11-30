67233b865912f15c04be2d2cb953e9253e8eaee1
danielterziev92 <daniel.st.terziev@gmail.com>
2023-11-30 13:56:52 +0200

    Update create user view

--- ReactJS/FinalProject/server/server/user_app/views.py
+++ ReactJS/FinalProject/server/server/user_app/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics as api_views
+from rest_framework import generics as api_views, serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -14,6 +14,23 @@ class AppTokenObtainPairView(TokenObtainPairView):
 class UserCreateView(api_views.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def create(self, validated_data):
+        serializer = self.get_serializer(data=self.request.data)
+        username = serializer.initial_data.get('username')
+        email = serializer.initial_data.get('email')
+
+        existing_user_by_username = User.objects.filter(username=username).exists()
+        existing_user_by_email = User.objects.filter(email=email).exists()
+
+        if existing_user_by_username:
+            raise serializers.ValidationError({'message': 'Потребител с това потребителско име съществува'})
+
+        if existing_user_by_email:
+            raise serializers.ValidationError({'message': 'Потребител с този емейл съществува'})
+
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
 class UserRetrieveUpdateView(api_views.RetrieveUpdateAPIView):
