02c8849d8ffa1c8b2f905a7701233aea39dd2acf
danielterziev92 <daniel.st.terziev@gmail.com>
2023-12-04 14:08:28 +0200

    Add to all CRUD operations for product authentication request

--- ReactJS/FinalProject/client/src/services/productService.js
+++ ReactJS/FinalProject/client/src/services/productService.js
@@ -2,29 +2,53 @@ import Urls from "../utils/Urls.js";
 
 import * as request from '../lib/request.js'
 import pathToUrl from "../utils/pathToUrl.js";
+import cookieManager from "../utils/cookieManager.js";
+import {tokenExpDays, tokenName} from "../contexts/AuthenticationContext.jsx";
+import tokenManager from "../utils/tokenManager.js";
 
 export const getAllProducts = async () => {
+    const refreshToken = cookieManager.getCookie(tokenName);
+    const newTokens = await tokenManager.getNewTokens(refreshToken);
+    cookieManager.setCookie(tokenName, newTokens.refresh, tokenExpDays);
+
     // await new Promise(resolve => setTimeout(resolve, 2000));
-    return request.get(Urls.products);
+    return request.get(Urls.products, null, newTokens.access);
 }
 
 export const getProductById = async (productId) => {
+    const refreshToken = cookieManager.getCookie(tokenName);
+    const newTokens = await tokenManager.getNewTokens(refreshToken);
+    console.log(newTokens)
+    cookieManager.setCookie(tokenName, newTokens.refresh, tokenExpDays);
+
     // await new Promise(resolve => setTimeout(resolve, 2000));
     const url = pathToUrl(Urls.productDetail, {id: productId});
-    return request.get(url);
+    return request.get(url, null, newTokens.access);
 }
 
 export const createProduct = async (data) => {
-    return request.post(Urls.productCreate, data);
+    const refreshToken = cookieManager.getCookie(tokenName);
+    const newTokens = await tokenManager.getNewTokens(refreshToken);
+    cookieManager.setCookie(tokenName, newTokens.refresh, tokenExpDays);
+
+    return request.post(Urls.productCreate, data, newTokens.access);
 }
 
 export const updateProductById = async (productId, data) => {
-    const url = pathToUrl(Urls.productDetail, {id: productId});
+    const refreshToken = cookieManager.getCookie(tokenName);
+    const newTokens = await tokenManager.getNewTokens(refreshToken);
+    cookieManager.setCookie(tokenName, newTokens.refresh, tokenExpDays);
+
+    const url = pathToUrl(Urls.productDetail, {id: productId}, newTokens.access);
     return request.put(url, data);
 }
 
 export const deleteProductById = async (productId) => {
-    const url = pathToUrl(Urls.productDetail, {id: productId});
+    const refreshToken = cookieManager.getCookie(tokenName);
+    const newTokens = await tokenManager.getNewTokens(refreshToken);
+    cookieManager.setCookie(tokenName, newTokens.refresh, tokenExpDays);
+
+    const url = pathToUrl(Urls.productDetail, {id: productId}, newTokens.access);
     return request.remove(url);
 }
 
