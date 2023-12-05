ce8c550f2c01851ec02b5d90c35abaa32a6cd5b6
danielterziev92 <daniel.st.terziev@gmail.com>
2023-12-05 23:20:59 +0200

    Fix but when logout

--- ReactJS/FinalProject/client/src/components/logout/Logout.jsx
+++ ReactJS/FinalProject/client/src/components/logout/Logout.jsx
@@ -1,16 +1,16 @@
 import {useContext, useLayoutEffect} from "react";
 
-import {AuthenticationContext} from "../../contexts/AuthenticationContext.jsx";
+import {AuthenticationContext, tokenName} from "../../contexts/AuthenticationContext.jsx";
 import {MessageContext} from "../../contexts/MessageContext.jsx";
 
 import cookieManager from "../../utils/cookieManager.js";
 
 export default function Logout() {
-    const {tokenName, updateAuthToken, updateUser} = useContext(AuthenticationContext);
+    const {updateAuthToken, updateUser} = useContext(AuthenticationContext);
     const {updateMessage, updateStatus} = useContext(MessageContext);
 
     useLayoutEffect(() => {
-        cookieManager.deleteCookie(tokenName.current);
+        cookieManager.deleteCookie(tokenName);
 
         updateAuthToken({});
         updateUser({});
