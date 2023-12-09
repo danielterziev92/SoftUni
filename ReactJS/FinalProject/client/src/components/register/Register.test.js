9db1a54ad40af87590a3f9ad433973ef5023fa9b
danielterziev92 <daniel.st.terziev@gmail.com>
2023-12-09 22:40:35 +0200

    add test to check is it have submit button with specific name for the form

--- ReactJS/FinalProject/client/src/components/register/Register.test.js
+++ ReactJS/FinalProject/client/src/components/register/Register.test.js
@@ -1,4 +1,4 @@
-import {render, screen,} from '@testing-library/react';
+import {fireEvent, render, screen,} from '@testing-library/react';
 import {BrowserRouter} from "react-router-dom";
 import Register from "./Register.jsx";
 import AuthenticationProvider from "../../contexts/AuthenticationContext.jsx";
@@ -15,14 +15,18 @@ const MockingRegisterComponent = () => {
 };
 
 describe('Test Register Component', () => {
+    const onSubmitHandler = jest.fn();
+
     test('have form element to be in the document', () => {
         render(<MockingRegisterComponent/>);
 
         expect(screen.getByRole('form', {name: 'register-form'})).toBeInTheDocument();
     });
 
-    // test('have onSubmit function in form element', () => {
-    //     const onSubmitHandler = jest.fn();
-    //
-    // });
+    test('have submit element to be in the document', () => {
+        render(<MockingRegisterComponent/>);
+
+        expect(screen.getByRole('button', {name: 'Регистрация'})).toBeInTheDocument();
+    });
+
 });
