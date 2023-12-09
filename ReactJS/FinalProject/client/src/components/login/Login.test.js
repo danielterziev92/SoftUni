a5688d213825117c979c72178310f7940f80e8c1
danielterziev92 <daniel.st.terziev@gmail.com>
2023-12-09 21:29:43 +0200

    Make test when user successful login, then redirect to specific page

--- ReactJS/FinalProject/client/src/components/login/Login.test.js
+++ ReactJS/FinalProject/client/src/components/login/Login.test.js
@@ -1,10 +1,23 @@
-import {render, screen} from "@testing-library/react";
+import {render, screen, fireEvent, waitFor} from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+import fetchMock from 'jest-fetch-mock';
 
-import {BrowserRouter, Form} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 import Login, {FormInformation} from "./Login.jsx";
 
 import AuthenticationProvider from "../../contexts/AuthenticationContext.jsx";
+import MessageProvider from "../../contexts/MessageContext.jsx";
+
+import Paths from "../../utils/Paths.js";
+
+fetchMock.enableMocks();
+
+jest.mock('react', () => ({
+    ...jest.requireActual('react'),
+    useEffect: jest.fn(),
+    useLayoutEffect: jest.fn(),
+}));
 
 describe('Test Login Component when', () => {
     test('when user is not authenticated', () => {
@@ -16,8 +29,6 @@ describe('Test Login Component when', () => {
             </BrowserRouter>
         );
 
-        screen.debug();
-
         const formElement = screen.getByTestId('login-form');
         const usernameLabel = screen.getByLabelText(FormInformation.username.label);
         const passwordInput = screen.getByLabelText(FormInformation.password.label);
@@ -25,5 +36,37 @@ describe('Test Login Component when', () => {
         expect(formElement).toBeInTheDocument();
         expect(usernameLabel).toBeInTheDocument();
         expect(passwordInput).toBeInTheDocument();
+    });
+
+
+    test('successful login and redirects to a specific page', async () => {
+
+        fetchMock.mockResolvedValueOnce({ /* mock response with token */});
+
+        render(
+            <BrowserRouter>
+                <MessageProvider setIsMessageBoxShow={() => {
+                }} isMessageBoxShow={null}>
+                    <AuthenticationProvider setIsLogin={jest.fn()} isLogin={false}>
+                        <Login/>
+                    </AuthenticationProvider>
+                </MessageProvider>
+            </BrowserRouter>
+        );
+
+        const usernameInput = screen.getByLabelText('Потребителско име');
+        const passwordInput = screen.getByLabelText('Парола');
+
+        const formElement = screen.getByTestId('login-form');
+
+        await userEvent.type(usernameInput, 'React');
+        await userEvent.type(passwordInput, 'React!23');
+
+        fireEvent.submit(formElement);
+
+        await waitFor(() => {
+            expect(window.location.pathname).toBe(Paths.afterLogin);
+        });
+
     });
 });
