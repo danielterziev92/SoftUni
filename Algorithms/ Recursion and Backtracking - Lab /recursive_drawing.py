6433d6c12e26e55c7730d70d25c600d49cae57b1
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-19 13:18:51 +0200

    I rationalized the solution of the tasks

--- Algorithms/ Recursion and Backtracking - Lab /recursive_drawing.py
+++ Algorithms/ Recursion and Backtracking - Lab /recursive_drawing.py
@@ -7,33 +7,14 @@ test2 = '5'
 sys.stdin = StringIO(test2)
 
 
-def get_symbol_count(numb, direction):
-    symbol = '*' if direction == 'down' else '#'
+def drawing(numb):
+    if numb == 0:
+        return
 
-    return numb * symbol
-
-
-def drawing(numb, idx=0, direction='down', result=list()) -> list:
-    if idx == 0:
-        idx = numb
-
-    if idx == 1 and direction == 'down':
-        direction = 'up'
-        result.append(get_symbol_count(idx, 'down'))
-        return drawing(numb, idx, direction, result)
-
-    elif idx == numb and direction == 'up':
-        result.append(get_symbol_count(idx, direction))
-        return result
-
-    else:
-        result.append(get_symbol_count(idx, direction))
-
-    if direction == 'down':
-        return drawing(numb, idx - 1, direction, result)
-    else:
-        return drawing(numb, idx + 1, direction, result)
+    print('*' * numb)
+    drawing(numb - 1)
+    print('#' * numb)
 
 
 input_numb = int(input())
-[print(row) for row in drawing(input_numb)]
+drawing(input_numb)
--- Algorithms/ Recursion and Backtracking - Lab /recursive_factorial.py
+++ Algorithms/ Recursion and Backtracking - Lab /recursive_factorial.py
@@ -3,15 +3,16 @@ from io import StringIO
 
 test1 = '5'
 test2 = '10'
+test3 = '0'
 
 sys.stdin = StringIO(test2)
 
 
-def calculate_factorial(numb, idx=1):
-    if numb == idx:
-        return idx
+def calculate_factorial(numb):
+    if numb == 0:
+        return 1
 
-    return idx * calculate_factorial(numb, idx + 1)
+    return numb * calculate_factorial(numb - 1)
 
 
 input_numb = int(input())
