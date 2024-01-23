edc159ce235503619ea307923c14b72a0a21bc29
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-23 13:34:55 +0200

    Add solution for task: Reverse Array

--- Algorithms/Recursion and Backtracking - Exercise/reverse_array.py
+++ Algorithms/Recursion and Backtracking - Exercise/reverse_array.py
@@ -6,13 +6,13 @@ test1 = '1 2 3 4 5 6'
 sys.stdin = StringIO(test1)
 
 
-def reverse_array(arr, idx, rev_arr):
+def print_reverse_array(arr, idx):
     if idx == len(arr):
         return
 
-    reverse_array(arr, idx + 1, rev_arr)
+    print_reverse_array(arr, idx + 1)
 
     print(arr[idx], end=' ')
 
 
-reverse_array(input().split(' '), 0, [])
+print_reverse_array(input().split(' '), 0)
