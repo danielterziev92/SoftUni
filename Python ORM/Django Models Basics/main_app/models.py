16f57ee6275dc38f8093d58ed3bb88f5a2675f88
danielterziev92 <daniel.st.terziev@gmail.com>
2024-03-17 16:16:51 +0200

    Add first migration. Add module: psycopg2 to the project and in requirements.txt. Add second model Deparment.

--- Python ORM/Django Models Basics/main_app/models.py
+++ Python ORM/Django Models Basics/main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Employee(models.Model):
@@ -30,6 +31,49 @@ class Employee(models.Model):
 
     created_on = models.DateTimeField(
         auto_now_add=True,
+        null=False,
+        blank=False,
+    )
+
+
+class Department(models.Model):
+    class LocationTowns(models.TextChoices):
+        SOFIA = "Sofia", _("Sofia")
+        PLOVDIV = "Plovdiv", _("Plovdiv")
+        BURGAS = "Burgas", _("Burgas")
+        VARNA = "Varna", _("Varna")
+
+    code = models.CharField(
+        max_length=4,
+        primary_key=True,
+        unique=True,
+        null=False,
+        blank=False,
+    )
+
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        null=False,
+        blank=False,
+    )
+
+    employees_count = models.PositiveIntegerField(
+        default=1,
+        null=False,
+        blank=False,
+        verbose_name='Employees Count',
+    )
+
+    location = models.CharField(
+        max_length=20,
+        choices=LocationTowns.choices,
+        null=False,
+        blank=False,
+    )
+
+    last_edited_on = models.DateTimeField(
+        auto_now=True,
         null=False,
         blank=False,
     )
