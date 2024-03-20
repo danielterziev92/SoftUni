1686db5ff3fa37ea338d85741e251e6f4a5da4a6
danielterziev92 <daniel.st.terziev@gmail.com>
2024-03-20 13:08:07 +0200

    Add solution for third taks of exercise

--- Python ORM/Django Models Basics Exercise/main_app/models.py
+++ Python ORM/Django Models Basics Exercise/main_app/models.py
@@ -26,3 +26,55 @@ class Blog(models.Model):
         null=False,
         blank=False,
     )
+
+
+class WeatherForecast(models.Model):
+    date = models.DateField(
+        null=False,
+        blank=False,
+    )
+
+    temperature = models.FloatField(
+        null=False,
+        blank=False,
+    )
+
+    humidity = models.FloatField(
+        null=False,
+        blank=False,
+    )
+
+    precipitation = models.FloatField(
+        null=False,
+        blank=False,
+    )
+
+
+class Recipe(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        null=False,
+        blank=False,
+    )
+
+    description = models.TextField(
+        null=False,
+        blank=False,
+    )
+
+    ingredients = models.TextField(
+        null=False,
+        blank=False,
+    )
+
+    cook_time = models.PositiveIntegerField(
+        null=False,
+        blank=False,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now=True,
+        null=False,
+        blank=False,
+    )
