24333fa8f1b6ead4a2d6bc711b1311852059bf8a
danielterziev92 <daniel.st.terziev@gmail.com>
2024-03-22 16:11:11 +0200

    Add solution for fourth task of Migrations and Django Admin - Exercise

--- Python ORM/Migrations and Django Admin - Exercise/main_app/admin.py
+++ Python ORM/Migrations and Django Admin - Exercise/main_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from main_app.models import EventRegistration, Movie
+
+
+@admin.register(EventRegistration)
+class EventRegistrationAdmin(admin.ModelAdmin):
+    list_display = ('event_name', 'participant_name', 'registration_date',)
+    list_filter = ('event_name', 'registration_date',)
+    search_fields = ('event_name', 'participant_name',)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'director', 'release_year', 'genre',)
+    list_filter = ('release_year', 'genre',)
+    search_fields = ('title', 'director',)
--- Python ORM/Migrations and Django Admin - Exercise/main_app/models.py
+++ Python ORM/Migrations and Django Admin - Exercise/main_app/models.py
@@ -21,3 +21,53 @@ class UniqueBrands(models.Model):
         null=False,
         blank=False,
     )
+
+
+class EventRegistration(models.Model):
+    event_name = models.CharField(
+        max_length=60,
+        null=False,
+        blank=False,
+    )
+
+    participant_name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+    )
+
+    registration_date = models.DateField(
+        null=False,
+        blank=False,
+    )
+
+    def __str__(self):
+        return f'{self.participant_name} - {self.event_name}'
+
+
+class Movie(models.Model):
+    title = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+    )
+
+    director = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+    )
+
+    release_year = models.PositiveIntegerField(
+        null=False,
+        blank=False,
+    )
+
+    genre = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+    )
+
+    def __str__(self):
+        return f'Movie "{self.title}" by {self.director}'
