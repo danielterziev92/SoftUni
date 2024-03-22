b828c31353236cbc16705b9cbd2b59027cafc26b
danielterziev92 <daniel.st.terziev@gmail.com>
2024-03-22 14:18:11 +0200

    Add solution for Migtrations and Django Admin - Lab

---
+++ Python ORM/Migrations and Django Admin - Lab/main_app/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from main_app.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'created_on',)
+    search_fields = ('name', 'category', 'supplier',)
+    list_filter = ('category', 'supplier',)
+    fieldsets = (
+        ('General Information', {
+            'fields': ('name', 'description', 'price', 'barcode',)
+        }),
+        ('Categorization', {
+            'fields': ('category', 'supplier',)
+        })
+    )
+    date_field = 'created_on'
