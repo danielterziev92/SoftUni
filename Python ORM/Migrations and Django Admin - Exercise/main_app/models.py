c0420928ea58e470c4d876292e8f71e5782276d6
danielterziev92 <daniel.st.terziev@gmail.com>
2024-03-22 14:42:54 +0200

    Add solution for first task of Migrations and Django Admin - Exercise

--- Python ORM/Migrations and Django Admin - Exercise/main_app/models.py
+++ Python ORM/Migrations and Django Admin - Exercise/main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Shoe(models.Model):
+    brand = models.CharField(
+        max_length=25,
+        null=False,
+        blank=False,
+    )
+
+    size = models.PositiveIntegerField(
+        null=False,
+        blank=False,
+    )
+
+
+class UniqueBrands(models.Model):
+    brand_name = models.CharField(
+        max_length=25,
+        unique=True,
+        null=False,
+        blank=False,
+    )
--- Python ORM/Migrations and Django Admin - Exercise/orm_skeleton/settings.py
+++ Python ORM/Migrations and Django Admin - Exercise/orm_skeleton/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
