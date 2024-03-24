c66f0e02aa51b7ee3462bfe6f2fd825287207de9
danielterziev92 <daniel.st.terziev@gmail.com>
2024-03-24 17:02:03 +0200

    Add solution for sixth task of Migrations and Django Admin - Exercise

--- Python ORM/Migrations and Django Admin - Exercise/main_app/admin.py
+++ Python ORM/Migrations and Django Admin - Exercise/main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from main_app.models import EventRegistration, Movie, Student
+from main_app.models import EventRegistration, Movie, Student, Supplier
 
 
 @admin.register(EventRegistration)
@@ -29,4 +29,17 @@ class StudentAdmin(admin.ModelAdmin):
         ('Academic Information', {
             'fields': ('grade',)
         })
+    )
+
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone',)
+    list_filter = ('name', 'phone',)
+    search_fields = ('email', 'contact_person', 'phone',)
+    list_per_page = 20
+    fieldsets = (
+        ('Information', {
+            'fields': ('name', 'contact_person', 'email', 'address',)
+        }),
     )
