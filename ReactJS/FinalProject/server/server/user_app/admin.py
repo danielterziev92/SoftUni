aba8c64332d8d2fa14e121bdb2c53e5d22bf16d8
danielterziev92 <daniel.st.terziev@gmail.com>
2024-03-26 13:08:11 +0200

    Create company members

--- ReactJS/FinalProject/server/server/user_app/admin.py
+++ ReactJS/FinalProject/server/server/user_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from server.user_app.models import UserApp
+from server.user_app.models import UserApp, UserBaseInfo, Company
 
 
 @admin.register(UserApp)
@@ -16,10 +16,46 @@ class UserAppAdmin(admin.ModelAdmin):
         }),
         ('Permissions', {
             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions',)
-        })
+        }),
     )
 
     def get_groups(self, obj):
         return ", ".join([group.name for group in obj.groups.all()])
 
     get_groups.short_description = 'Groups'
+
+
+@admin.register(UserBaseInfo)
+class UserBaseInfoAdmin(admin.ModelAdmin):
+    list_display = ('get_email', 'first_name', 'last_name', 'get_phone_number')
+    readonly_fields = ('get_email', 'user')
+
+    fieldsets = (
+        ('User Profile', {
+            'fields': ('user',)
+        }),
+        ('Personal Info', {
+            'fields': ('first_name', 'last_name', 'phone')
+        })
+    )
+
+    def get_email(self, obj):
+        return obj.user.email
+
+    def get_phone_number(self, obj):
+        return f'+359{obj.phone}'
+
+    get_email.short_description = 'email'
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['get_email', 'user']
+        else:
+            return []
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'town',)
+    search_fields = ('name', 'owner',)
+    list_per_page = 20
--- ReactJS/FinalProject/server/server/user_app/models.py
+++ ReactJS/FinalProject/server/server/user_app/models.py
@@ -1,7 +1,11 @@
-from django.db import models
 from django.contrib.auth import models as auth_models
+from django.db import models
+from django.core.validators import MinLengthValidator
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from server.user_app.manager import UserAppManager
+from server.utils.models_mixin import DateInfoMixin
 
 
 class UserApp(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
@@ -37,3 +41,100 @@ class UserApp(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class UserBaseInfo(models.Model):
+    FIRST_NAME_MAX_LENGTH = 30
+    LAST_NAME_MAX_LENGTH = 30
+    PHONE_MIN_LENGTH = 9
+    PHONE_MAX_LENGTH = 9
+
+    first_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    last_name = models.CharField(
+        max_length=LAST_NAME_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    phone = models.CharField(
+        max_length=PHONE_MAX_LENGTH,
+        validators=(MinLengthValidator(PHONE_MIN_LENGTH),),
+        null=False,
+        blank=False,
+    )
+
+    user = models.ForeignKey(
+        UserApp,
+        on_delete=models.CASCADE,
+        related_name='users'
+    )
+
+    class Meta:
+        verbose_name_plural = 'Users Base Information'
+
+    def __str__(self):
+        return self.user.email
+
+    def clean(self):
+        is_user_exist = UserBaseInfo.objects.filter(user=self.user).first()
+
+        if is_user_exist and is_user_exist != self:
+            raise ValidationError(_('A UserBaseInfo object already exists for this user.'))
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+
+        super().save(*args, **kwargs)
+
+
+class Company(DateInfoMixin):
+    NAME_MAX_LENGTH = 100
+    TOWN_MAX_LENGTH = 50
+    ADDRESS_MAX_LENGTH = 200
+
+    name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    town = models.CharField(
+        max_length=TOWN_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    address = models.CharField(
+        max_length=ADDRESS_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    owner = models.ForeignKey(
+        UserBaseInfo,
+        on_delete=models.CASCADE,
+        related_name='owner'
+    )
+
+    class Meta:
+        verbose_name_plural = 'Companies'
+
+    def __str__(self):
+        return self.name
+
+
+class CompanyMembers(models.Model):
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+        related_name='Company'
+    )
+
+    members = models.ManyToManyField(
+        UserBaseInfo
+    )
