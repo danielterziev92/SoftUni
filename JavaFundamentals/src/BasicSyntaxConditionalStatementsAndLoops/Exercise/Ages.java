56af4f079a2e94a27e013d3d72473bcb30a68025
danielterziev92 <daniel.st.terziev@gmail.com>
2025-01-09 17:25:28 +0200

    Add age group classification program
    The program takes an integer input for age and determines the person's age group based on the following criteria:
    - 0-2 years: baby
    - 3-13 years: child
    - 14-19 years: teenager
    - 20-65 years: adult
    - over 65 years: elder

---
+++ JavaFundamentals/src/BasicSyntaxConditionalStatementsAndLoops/Exercise/Ages.java
@@ -0,0 +1,23 @@
+package BasicSyntaxConditionalStatementsAndLoops.Exercise;
+
+import java.util.Scanner;
+
+public class Ages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int age = Integer.parseInt(scanner.nextLine());
+
+        if (age >= 0 && age <= 2) {
+            System.out.println("baby");
+        } else if (age > 2 && age <= 13) {
+            System.out.println("child");
+        } else if (age > 13 && age <= 19) {
+            System.out.println("teenager");
+        } else if (age > 19 && age <= 65) {
+            System.out.println("adult");
+        } else {
+            System.out.println("elder");
+        }
+    }
+}
