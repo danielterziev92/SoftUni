ef19c65a68e35c1f8d8fff54ba6735ad2f450f04
Daniel Terziev <daniel.st.terziev@gmail.com>
2025-01-20 10:36:48 +0200

    Add MagicSum class to find pairs with specific sum
    - Parse input array using streams
    - Implement nested loops to check all possible pairs
    - Compare pair sums against target sum
    - Display pairs that match the target sum

---
+++ JavaFundamentals/src/Arrays/Exercise/MagicSum.java
@@ -0,0 +1,24 @@
+package Arrays.Exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MagicSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == sum) {
+                    System.out.println(arr[i] + " " + arr[j]);
+                }
+            }
+        }
+    }
+}
